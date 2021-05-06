90576cce7efa96d1c5d2c764ef05c54b6b75650e
Hasmik Varosyan <hasvart@gmail.com>
2021-05-06 13:36:26 -0500

    created a board with cards

--- script.js
+++ script.js
@@ -52,4 +52,20 @@ document.addEventListener('DOMContentLoaded', () =>{
          }
   ]
 
+const grid = document.querySelector('.grid');
+  
+//creating board
+//using a for loop to loop over the card array and for each card creating an image element
 
+function createBoard(){
+for (let i = 0; i< cardArray.length; i++) {
+   const card = document.createElement('img')
+   card.setAttribute('src', 'images/blank.png')
+   card.setAttribute('data-id', i)
+   //card.addEventListener('click', flipCard)
+   grid.appendChild(card)
+ }
+}
+
+createBoard()
+})
